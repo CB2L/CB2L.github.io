c82e1e4c2b3b7ffae933166854b854b14ac9000a
Computational Biology & Bioinformatics Laboratory <cb2labs@gmail.com>
2016-05-09 14:41:01 +0530

    Initial commit:100:
    Path: config/watch.js
    Watch: Run predefined tasks whenever watched file patterns are added, changed or deleted.
    Usage: $ npm install grunt-contrib-watch --save-dev

---
+++ config/watch.js
@@ -0,0 +1,24 @@
+/*!
+ * CBBL®
+ * ________________________________________________________________________
+ * Grunt, The JavaScript Task Runner - http://gruntjs.com/.
+ * ~~~~~
+ * Watch               : @require =========================================
+ *   |------------------ HTML/SASS/CSS/JavaScript/JSON
+ * grunt-contrib-watch : https://github.com/gruntjs/grunt-contrib-watch
+ * ________________________________________________________________________
+ * Usage               : $ npm install grunt-contrib-watch --save-dev
+ * Usage               : $ grunt serve
+ * ________________________________________________________________________
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/]
+ * @copyright : Sequømics Corporation [http://sequomics.com/]
+ * ________________________________________________________________________
+ * @date      : 09-May-2016
+ * @license   : Apache, version 2.0
+ * @require   : Node.js®
+ * @require   : NPM
+ * @require   : Grunt
+ * ________________________________________________________________________
+ */
