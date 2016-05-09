95071da782adddd80a209bf46df97a4a76fc2b35
Computational Biology & Bioinformatics Laboratory <cb2labs@gmail.com>
2016-05-09 14:34:03 +0530

    Initial commit:100:
    Path: config/update.js
    Why? – To automate the updating of our "package.json" packages with a grunt task.
    Usage: $ npm install grunt-dev-update --save-dev

---
+++ config/update.js
@@ -0,0 +1,22 @@
+/*!
+ * CBBL®
+ * ________________________________________________________________________
+ * Grunt, The JavaScript Task Runner - http://gruntjs.com/.
+ * ~~~~~~
+ * Update           : @require ============================================
+ *   |-------------- Update devDependencies and dependencies automatically.
+ * grunt-dev-update : https://github.com/pgilad/grunt-dev-update
+ * Usage            : $ npm install grunt-dev-update --save-dev
+ * ________________________________________________________________________
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/]
+ * @copyright : Sequømics Corporation [http://sequomics.com/]
+ * ________________________________________________________________________
+ * @date      : 09-May-2016
+ * @license   : Apache, version 2.0
+ * @require   : Node.js®
+ * @require   : NPM
+ * @require   : Grunt
+ * ________________________________________________________________________
+ */
