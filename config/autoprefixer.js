8c164f2c3cbdd0c3ae70aa0dde22317c80c56731
Computational Biology & Bioinformatics Laboratory <cb2labs@gmail.com>
2016-05-09 13:24:09 +0530

    Initial commit
    Path: config/autoprefixer.js
    Usage:
    1. $ npm install grunt-autoprefixer --save-dev
    2. $ npm install grunt-postcss --save-dev

---
+++ config/autoprefixer.js
@@ -0,0 +1,24 @@
+/*!
+ * CBBL®
+ * __________________________________________________________________
+ * Grunt, The JavaScript Task Runner - http://gruntjs.com/.
+ * ~~~~~~~~~~~~
+ * AutoPrefixer       :==============================================
+ * grunt-autoprefixer : https://github.com/nDmitry/grunt-autoprefixer
+ * Usage              : $ npm install grunt-autoprefixer --save-dev
+ * !
+ * grunt-postcss      : https://github.com/nDmitry/grunt-postcss
+ * Usage              : $ npm install grunt-postcss --save-dev
+ * __________________________________________________________________
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/]
+ * @copyright : Sequømics Corporation [http://sequomics.com/]
+ * __________________________________________________________________
+ * @date      : 09-May-2016
+ * @license   : Apache, version 2.0
+ * @require   : Node.js®
+ * @require   : NPM
+ * @require   : Grunt
+ * __________________________________________________________________
+ */
