36b4767cb6634a2cbd55b45c8ec4ad2cd6f9c521
Computational Biology & Bioinformatics Laboratory <cb2labs@gmail.com>
2016-05-09 16:26:05 +0530

    Initial commit
    setup.py, the setup script is the centre of all activity in building, distributing, and installing modules.
    Copyright © 2014 - 2016, Sequømics Corporation. All rights reserved.

---
+++ setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# ------------------------------------------------------------------------
+# Copyright © 2014 - 2016, Sequømics Corporation. All rights reserved.
+# ------------------------------------------------------------------------
+# Licensed under the Apache License, Version 2.0, (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at:
+#
+#       	    http://www.apache.org/licenses/LICENSE-2.0
+#                           	    or
+#     https://github.com/CB2L/CB2L.github.io/blob/master/LICENSE
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS-IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ========================================================================
