ef7189d3f42b4309b4d3b36caa6cbd6963f75121
Computational Biology & Bioinformatics Laboratory <cb2labs@gmail.com>
2016-05-09 13:16:09 +0530

    Initial commit
    Configurations for Build/Test/Release [BTR] of CBBL®.

---
+++ cbbl.config.js
@@ -0,0 +1,12 @@
+/*!
+ * CBBL®
+ * ______________________________________________________________
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/]
+ * @copyright : Sequømics Corporation [http://sequomics.com/]
+ * ______________________________________________________________
+ * @date      : 09-May-2016
+ * @license   : Apache, version 2.0
+ * ______________________________________________________________
+ */
