ef40aa3fdf2aa96b35d40f43b1db16c7e695216b
Computational Biology & Bioinformatics Laboratory <cb2labs@gmail.com>
2016-05-09 13:28:01 +0530

    Initial commit
    Path: config/bower.js
    grunt-wiredep: Inject Bower packages into our source code with Grunt.
    Usage: $ npm install grunt-wiredep --save-dev

---
+++ config/bower.js
@@ -0,0 +1,22 @@
+/*!
+ * CBBL®
+ * ____________________________________________________________________
+ * Grunt, The JavaScript Task Runner - http://gruntjs.com/.
+ * ~~~~~
+ * Bower          : ===================================================
+ * grunt-wiredep  : https://github.com/stephenplusplus/grunt-wiredep
+ * Usage          : $ npm install grunt-wiredep --save-dev
+ * @require       : .bowerrc and bower.json
+ * ____________________________________________________________________
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/]
+ * @copyright : Sequømics Corporation [http://sequomics.com/]
+ * ____________________________________________________________________
+ * @date      : 09-May-2016
+ * @license   : Apache, version 2.0
+ * @require   : Node.js®
+ * @require   : NPM
+ * @require   : Grunt
+ * ____________________________________________________________________
+ */
