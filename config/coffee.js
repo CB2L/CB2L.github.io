e8fc6138d03d3e615212073084db58fb9703b573
Computational Biology & Bioinformatics Laboratory <cb2labs@gmail.com>
2016-05-09 13:35:49 +0530

    Initial commit
    Path: config/coffee.js
    grunt-contrib-coffee: Compile CoffeeScript files to JavaScript.
    Usage:
    1. $ npm install grunt-contrib-coffee --save-dev
    2. $ npm install grunt-coffeelint --save-dev

---
+++ config/coffee.js
@@ -0,0 +1,32 @@
+/*!
+ * CBBL®
+ * ___________________________________________________________________________
+ * Grunt, The JavaScript Task Runner - http://gruntjs.com/.
+ * ~~~~~~
+ * Coffee                 : @require =========================================
+ * --> 1.
+ * grunt-contrib-coffee	  : https://github.com/gruntjs/grunt-contrib-coffee
+ * Usage              	  : $ npm install grunt-contrib-coffee --save-dev
+ * !
+ * coffee-script          - https://github.com/jashkenas/coffeescript
+ * ___________________________________________________________________________
+ * --> 2.
+ * grunt-coffeelint       : https://github.com/vojtajina/grunt-coffeelint
+ * Usage                  : $ npm install grunt-coffeelint --save-dev
+ * @require               : rules/.coffeelint.json
+ * !
+ * coffeelint             - https://github.com/clutchski/coffeelint
+ * coffeelint-stylish 	  - https://github.com/janraasch/coffeelint-stylish
+ * ___________________________________________________________________________
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/]
+ * @copyright : Sequømics Corporation [http://sequomics.com/]
+ * ___________________________________________________________________________
+ * @date      : 09-May-2016
+ * @license   : Apache, version 2.0
+ * @require   : Node.js®
+ * @require   : NPM
+ * @require   : Grunt
+ * ___________________________________________________________________________
+ */
