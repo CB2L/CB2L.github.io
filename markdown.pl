89217f7c3bbad8c0eebdced530b91ed1d55d103c
Computational Biology & Bioinformatics Laboratory <cb2labs@gmail.com>
2016-05-09 16:14:20 +0530

    Initial commit
    Markdown 1.0.1 (18 KB) — 17 Dec 2004.
    Markdown is a text-to-HTML conversion tool for web writers.
    Markdown allows you to write using an easy-to-read, easy-to-write plain text format, then convert it to structurally valid XHTML (or HTML).
    Help: http://daringfireball.net/projects/markdown/.

---
+++ markdown.pl
@@ -0,0 +1,8 @@
+#!/usr/bin/perl
+
+
+# CB2L®     : Computational Biology and Bioinformatics Laboratory.
+# ----------------------------------------------------------------
+# Author    : Prabhat Kumar [http://prabhatkumar.org/].
+# Copyright : Sequømics Corporation [http://sequomics.com/].
+# ----------------------------------------------------------------
