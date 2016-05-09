9f3cc026383cb8750943c9dcdffa9f2ade743a48
Computational Biology & Bioinformatics Laboratory <cb2labs@gmail.com>
2016-05-09 14:18:31 +0530

    Initial commit
    Path: config/concat.js
    grunt-contrib-concat: Concatenating multiple JavaScript source files.
    Usage: $ npm install grunt-contrib-concat --save-dev

---
+++ config/concat.js
@@ -0,0 +1,20 @@
+/*!
+ * CBBL®
+ * ______________________________________________________________________
+ * Grunt, The JavaScript Task Runner - http://gruntjs.com/.
+ * ~~~~~~~~~~~~~~~~~~~
+ * grunt-contrib-concat : https://github.com/gruntjs/grunt-contrib-concat
+ * Usage                : $ npm install grunt-contrib-concat --save-dev
+ * ______________________________________________________________________
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/]
+ * @copyright : Sequømics Corporation [http://sequomics.com/]
+ * ______________________________________________________________________
+ * @date      : 09-May-2016
+ * @license   : Apache, version 2.0
+ * @require   : Node.js®
+ * @require   : NPM
+ * @require   : Grunt
+ * ______________________________________________________________________
+ */
