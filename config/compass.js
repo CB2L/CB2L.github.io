8435d841da14d210a317dcdce702dcb438bbecf6
Computational Biology & Bioinformatics Laboratory <cb2labs@gmail.com>
2016-05-09 14:15:39 +0530

    Initial commit
    Path: config/compass.js
    Usage: $ npm install grunt-contrib-compass --save-dev

---
+++ config/compass.js
@@ -0,0 +1,20 @@
+/*!
+ * CBBL®
+ * ________________________________________________________________________
+ * Grunt, The JavaScript Task Runner - http://gruntjs.com/.
+ * ~~~~~~~~~~~~~~~~~~~~~
+ * grunt-contrib-compass : https://github.com/gruntjs/grunt-contrib-compass
+ * Usage                 : $ npm install grunt-contrib-compass --save-dev
+ * ________________________________________________________________________
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/]
+ * @copyright : Sequømics Corporation [http://sequomics.com/]
+ * ________________________________________________________________________
+ * @date      : 09-May-2016
+ * @license   : Apache, version 2.0
+ * @require   : Node.js®
+ * @require   : NPM
+ * @require   : Grunt
+ * ________________________________________________________________________
+ */
