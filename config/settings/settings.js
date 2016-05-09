df2b774af66802dc45f85ad11a1df4c08fecc7b0
Computational Biology & Bioinformatics Laboratory <cb2labs@gmail.com>
2016-05-09 14:49:22 +0530

    Initial commit
    CBBL®: Application settings to export in to Build System.

---
+++ config/settings/settings.js
@@ -0,0 +1,9 @@
+/*!
+ * CBBL®      : Application Settings [+]
+ * ______________________________________________________________
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/]
+ * @copyright : Sequømics Corporation [http://sequomics.com/]
+ * ______________________________________________________________
+ */
