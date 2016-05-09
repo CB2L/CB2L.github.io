56eda8f9fa4d9a432c8df8bf95c2474f8cff1ea1
Computational Biology & Bioinformatics Laboratory <cb2labs@gmail.com>
2016-05-09 14:36:40 +0530

    Initial commit
    Path: config/wait.js
    Wait: Grunt wait Task for e2e testing.

---
+++ config/wait.js
@@ -0,0 +1,19 @@
+/*!
+ * CBBL®
+ * ______________________________________________________________
+ * Grunt, The JavaScript Task Runner - http://gruntjs.com/.
+ * ~~~~
+ * Wait       : Grunt wait Task for "e2e" testing.
+ * ______________________________________________________________
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/]
+ * @copyright : Sequømics Corporation [http://sequomics.com/]
+ * ______________________________________________________________
+ * @date      : 09-May-2016
+ * @license   : Apache, version 2.0
+ * @require   : Node.js®
+ * @require   : NPM
+ * @require   : Grunt
+ * ______________________________________________________________
+ */
