9f61e00153a125e197e7f5600ee1b8f28c8f3b03
Computational Biology & Bioinformatics Laboratory <cb2labs@gmail.com>
2016-05-09 13:12:54 +0530

    Initial commit
    --/The Heart of Build System/-- of "CBBL®".
    Gruntfile.js, configuring the JavaScript Task Runner.
    Help: Help: https://github.com/gruntjs or http://gruntjs.com/ and http://gruntjs.com/configuring-tasks
    Usage: $ grunt build

---
+++ Gruntfile.js
@@ -0,0 +1,26 @@
+#!/usr/bin/env node
+
+
+/*!
+ * CBBL®
+ * Computational Biology and Bioinformatics Laboratory
+ * Υπολογιστικής Βιολογίας και Βιοπληροφορικής Εργαστήριο
+ * ______________________________________________________________
+ * Grunt, http://gruntjs.com/.
+ * The JavaScript Task Runner.
+ * ______________________________________________________________
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/]
+ * @copyright : Sequømics Corporation [http://sequomics.com/]
+ * ______________________________________________________________
+ * @date      : 09-May-2016
+ * @license   : Apache, version 2.0
+ * @require   : Node.js®
+ * @require   : NPM
+ * @require   : Grunt
+ * ______________________________________________________________
+ *
+ * --/The Heart of Build System/-- of "CBBL®".
+ * ______________________________________________________________
+ */
