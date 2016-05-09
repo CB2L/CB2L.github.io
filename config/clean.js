c5bd04f5369e1be30ad8965426298bb9726ecd07
Computational Biology & Bioinformatics Laboratory <cb2labs@gmail.com>
2016-05-09 13:30:59 +0530

    Initial commit
    Path: config/clean.js
    Usage: $ npm install grunt-contrib-clean --save-dev

---
+++ config/clean.js
@@ -0,0 +1,20 @@
+/*!
+ * CBBL®
+ * ____________________________________________________________________
+ * Grunt, The JavaScript Task Runner - http://gruntjs.com/.
+ * ~~~~~~~~~~~~~~~~~~~
+ * grunt-contrib-clean : https://github.com/gruntjs/grunt-contrib-clean
+ * Usage               : $ npm install grunt-contrib-clean --save-dev
+ * ____________________________________________________________________
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/]
+ * @copyright : Sequømics Corporation [http://sequomics.com/]
+ * ____________________________________________________________________
+ * @date      : 09-May-2016
+ * @license   : Apache, version 2.0
+ * @require   : Node.js®
+ * @require   : NPM
+ * @require   : Grunt
+ * ____________________________________________________________________
+ */
