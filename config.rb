8cd33b11e2c72e26f3bc54a8ea8725bd0fea5c2d
Computational Biology & Bioinformatics Laboratory <cb2labs@gmail.com>
2016-05-10 00:01:19 +0530

    Initial commit
    AXIX®: https://github.com/AXIX

--- config.rb
+++ config.rb
@@ -1,1 +1,20 @@
 #!/usr/bin/env ruby
+
+
+# AXIX® - https://github.com/AXIX
+# -----------------------------------------
+# From the Desk of Sequømics Corporation.
+# -----------------------------------------
+# @author     : Prabhat Kumar.
+# @copyright  : Sequømics Corporation.
+# @homepage   : http://sequomics.com/.
+# @require    : Ruby
+# @require    : Sass
+# @require    : Compass
+# @version    : 1.0.1
+# @build      : Everest
+# @date       : 10-May-2016
+# @license    : Apache, version 2.0
+# -----------------------------------------
+# Principle   : Standard Module Definition
+# -----------------------------------------
