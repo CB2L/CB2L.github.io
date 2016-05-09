129f88809c6ab071274ed9214587aec47e7ecc39
Computational Biology & Bioinformatics Laboratory <cb2labs@gmail.com>
2016-05-10 00:15:29 +0530

    Initial commit
    Compiling with Sourcemap for "CBBL®".
    Help: pass the --sourcemap compiler option for future debug.

--- config.rb
+++ config.rb
@@ -28,3 +28,41 @@
 #        @author : Prabhat Kumar         //
 #          @date : 10-May-2016           //
 # -------------------------------------- //
+
+# Sass - (3.4.21)     : Selective Steve
+# Compass - (v1.0.3)  : Polaris
+
+# Require compass plugin:
+require 'compass/import-once/activate'
+
+# Setting the root path of project:
+http_path       = "/"
+css_dir         = "source/core/style"
+sass_dir        = "source/core/scss"
+images_dir      = "source/core/media/images"
+javascripts_dir = "source/core/script"
+
+# Preferred output style here (can be overridden via the command line):
+# output_style = :expanded or :nested or :compact or :compressed
+output_style    = :expanded
+
+# To enable relative paths to assets via compass helper functions:
+relative_assets = true
+
+# To disable debugging comments that display the original location of the selectors:
+# options: true || false
+line_comments   = true
+
+# Preference for the indented syntax, to regenerate this project again,
+# Pass: --syntax sass, or uncomment this:
+# preferred_syntax = :sass
+# and then run:
+# sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
+
+# Usage: $ compass version
+# ------------------------
+# Usage: $ compass compile
+# Usage: $ compass watch
+# ------------------------
+# Usage: $ compass compile --production
+# Usage: $ compass compile --sourcemap
