f7757991bcca45cee5dc8a17784e02157d9ed2f5
Computational Biology & Bioinformatics Laboratory <cb2labs@gmail.com>
2016-05-09 15:14:01 +0530

    Initial commit
    env.sh, shell script to --/inform/-- environments of "CBBL®" in one go.
    Usage: $ sh env.sh

---
+++ env.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+
+# ============================ Sequømics® ============================
+#                                 CBBL®
+# Handcrafted by Prabhat Kumar, http://prabhatkumar.org/.
+# Copyright © 2014 - 2016, Sequømics Corporation. All rights reserved.
+# ====================================================================
