25317298d8fc289e6f9e2897e393fb2002dac5c8
Computational Biology & Bioinformatics Laboratory <cb2labs@gmail.com>
2016-05-09 13:19:59 +0530

    Initial commit
    Path: config/assembler.js
    Usage: $ npm install assemble --save-dev

---
+++ config/assembler.js
@@ -0,0 +1,21 @@
+/*!
+ * CBBL®
+ * ______________________________________________________________
+ * Grunt, The JavaScript Task Runner - http://gruntjs.com/.
+ * ~~~~~~~~~
+ * Assembler  : =================================================
+ * Assemble   : https://github.com/assemble/assemble
+ * Usage      : $ npm install assemble --save-dev
+ * ______________________________________________________________
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/]
+ * @copyright : Sequømics Corporation [http://sequomics.com/]
+ * ______________________________________________________________
+ * @date      : 09-May-2016
+ * @license   : Apache, version 2.0
+ * @require   : Node.js®
+ * @require   : NPM
+ * @require   : Grunt
+ * ______________________________________________________________
+ */
