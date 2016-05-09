d3994b8385f221a4c745e7f189fb0e51db95e957
Computational Biology & Bioinformatics Laboratory <cb2labs@gmail.com>
2016-05-09 14:30:00 +0530

    Initial commit
    Path: config/linter.js
    Usage:
    1. $ npm install grunt-htmlhint --save-dev
    2. $ npm install grunt-scss-lint --save-dev
    3. $ npm install grunt-contrib-csslint --save-dev
    4. $ npm install grunt-contrib-jshint --save-dev

---
+++ config/linter.js
@@ -0,0 +1,35 @@
+/*!
+ * CBBL®
+ * ___________________________________________________________________________
+ * Grunt, The JavaScript Task Runner - http://gruntjs.com/.
+ * ~~~~~~~~~
+ * Linter(s)              : @require =========================================
+ * --> 1.
+ * grunt-htmlhint         : https://github.com/yaniswang/grunt-htmlhint
+ * Usage              	  : $ npm install grunt-htmlhint --save-dev
+ * @require               : rules/.htmlhintrc
+ * --> 2.
+ * grunt-scss-lint        : https://github.com/ahmednuaman/grunt-scss-lint
+ * Usage                  : $ npm install grunt-scss-lint --save-dev
+ * @require               : rules/.scss-lint.yml
+ * --> 3.
+ * grunt-contrib-csslint  : https://github.com/gruntjs/grunt-contrib-csslint
+ * Usage              	  : $ npm install grunt-contrib-csslint --save-dev
+ * @require               : rules/.csslintrc
+ * --> 4.
+ * grunt-contrib-jshint   : https://github.com/gruntjs/grunt-contrib-jshint
+ * Usage              	  : $ npm install grunt-contrib-jshint --save-dev
+ * @require               : rules/.jshintrc
+ * ___________________________________________________________________________
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/]
+ * @copyright : Sequømics Corporation [http://sequomics.com/]
+ * ___________________________________________________________________________
+ * @date      : 09-May-2016
+ * @license   : Apache, version 2.0
+ * @require   : Node.js®
+ * @require   : NPM
+ * @require   : Grunt
+ * ___________________________________________________________________________
+ */
