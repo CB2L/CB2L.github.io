46c9a316b431cca08cb9290634cd5bbca5641420
Computational Biology & Bioinformatics Laboratory <cb2labs@gmail.com>
2016-05-10 18:08:21 +0530

    Initial commit
    Micro(μ)®: A very lightweight microscopic progress bar.
    Help: https://github.com/iamprabhat/micro

---
+++ source/core/script/plugin/microbar.js
@@ -0,0 +1,14 @@
+/*!
+ * ~~~~~~~~~
+ * Micro(μ)®  : A very lightweight microscopic progress bar.
+ * __________________________________________________________________________________
+ * @copyright : 2016, Prabhat Kumar, All Rights Reserved.
+ * @help      : https://github.com/iamprabhat/micro
+ * @license   : MIT
+ * __________________________________________________________________________________
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/].
+ * @copyright : Prabhat Kumar [http://prabhatkumar.org/].
+ * __________________________________________________________________________________
+ */
