3d4db20d283033a3917cccd8a7830c4d12f7541c
Computational Biology & Bioinformatics Laboratory <cb2labs@gmail.com>
2016-05-10 18:05:37 +0530

    Initial commit
    Letters®: An experimental plugin for animating SVG letters using Segment.
    Help: https://github.com/lmgonzalves/letters

---
+++ source/core/script/plugin/letters.js
@@ -0,0 +1,14 @@
+/*!
+ * ~~~~~~~~
+ * Letters®   : An experimental plugin for animating SVG letters using Segment.
+ * __________________________________________________________________________________
+ * @copyright : 2016, lmgonzalves, All Rights Reserved.
+ * @help      : https://github.com/lmgonzalves/letters
+ * @license   : MIT
+ * __________________________________________________________________________________
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/]
+ * @copyright : Sequømics Corporation [http://sequomics.com/]
+ * __________________________________________________________________________________
+ */
