9cc357a384669152a987d0ec38b8f5d3162aadd1
Computational Biology & Bioinformatics Laboratory <cb2labs@gmail.com>
2016-05-10 18:22:49 +0530

    Initial commit
    namespace®: A RequireJS plugin for collecting modules into a namespace.
    Help: https://github.com/atesgoral/requirejs-namespace-plugin

---
+++ source/core/script/helpers/namespace.js
@@ -0,0 +1,15 @@
+/*!
+ * ~~~~~~~~~~
+ * namespace® : An AMD plugin for Namespace.
+ * ____________________________________________________________________
+ * @copyright : 2013, Ates Goral. All Rights Reserved.
+ * @help      : https://github.com/atesgoral/requirejs-namespace-plugin
+ * @require   : requirejs
+ * @license   : MIT license.
+ * ____________________________________________________________________
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/]
+ * @copyright : Sequømics Corporation [http://sequomics.com/]
+ * ____________________________________________________________________
+ */
