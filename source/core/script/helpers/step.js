1138d502fe34c5912d6c101f6b57b15a3c36da08
Computational Biology & Bioinformatics Laboratory <cb2labs@gmail.com>
2016-05-10 21:04:23 +0530

    Initial commit
    step®: An AMD loader plugin for loaders like RequireJS that will load scripts in steps.
    This allows us to load traditional scripts that do not use AMD in a sequential fashion.
    Help: https://github.com/requirejs/step

---
+++ source/core/script/helpers/step.js
@@ -0,0 +1,15 @@
+/*!
+ * ~~~~~
+ * step®      : An AMD loader plugin for loading scripts in steps.
+ * __________________________________________________________________
+ * @copyright : 2010 - 2014, The Dojo Foundation All Rights Reserved.
+ * @help      : http://github.com/requirejs/text
+ * @require   : requirejs
+ * @license   : MIT or new BSD license.
+ * __________________________________________________________________
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/]
+ * @copyright : Sequømics Corporation [http://sequomics.com/]
+ * __________________________________________________________________
+ */
