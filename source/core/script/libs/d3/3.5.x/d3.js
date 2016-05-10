c513ebcc34ab656477096f1052fe590b2291f0bc
Computational Biology & Bioinformatics Laboratory <cb2labs@gmail.com>
2016-05-10 23:42:55 +0530

    Initial commit
    D3 - A JavaScript visualization library for HTML and SVG.
    Help: https://github.com/mbostock/d3 | https://d3js.org/.

---
+++ source/core/script/libs/d3/3.5.x/d3.js
@@ -0,0 +1,8 @@
+/*!
+ * D3 [Data-Driven Documents]
+ * A JavaScript visualization library for HTML and SVG.
+ * @copyright : 2010 - 2016, Michael Bostock.
+ * @help      : https://github.com/mbostock/d3 | http://bost.ocks.org/
+ * @license   : MIT
+ * ____________ https://d3js.org/ | https://github.com/mbostock
+ */
