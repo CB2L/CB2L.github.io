c54b0d4fbd262a937b1dda12b3417c63e9e61269
Computational Biology & Bioinformatics Laboratory <cb2labs@gmail.com>
2016-05-11 00:32:28 +0530

    Initial commit
    mo: motion graphics toolbelt for the web.
    Help: http://mojs.io/.

---
+++ source/core/script/libs/mo/0.174.4/mo.js
@@ -0,0 +1,8 @@
+/*!
+ * mo · js
+ * motion graphics toolbelt for the web.
+ * @copyright : 2015 - 2016, Oleg Solomka.
+ * @help      : https://github.com/legomushroom/mojs
+ * @license   : MIT
+ * ____________ http://mojs.io/ | http://mojs.io/tutorials/
+ */
