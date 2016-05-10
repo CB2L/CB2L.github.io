5fb23cbf0d45b012bcfa2dde3fcd32d3f5012340
Computational Biology & Bioinformatics Laboratory <cb2labs@gmail.com>
2016-05-11 00:18:38 +0530

    Initial commit
    jQuery – JavaScript library for DOM operations.
    Help: http://jquery.com/ | https://github.com/jquery/jquery

---
+++ source/core/script/libs/jquery/1.9.1/jquery.js
@@ -0,0 +1,8 @@
+/*!
+ * jQuery – JavaScript Library.
+ * JavaScript library for DOM operations.
+ * @copyright : 2005 - 2012, jQuery Foundation, Inc. and other contributors.
+ * @help      : https://github.com/jquery/jquery
+ * @license   : MIT
+ * ____________ http://jquery.com/ | https://jquery.org/license/
+ */
