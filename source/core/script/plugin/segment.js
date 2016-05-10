0b85f4e521bb35793ce8db06b725772fe8bf98b4
Computational Biology & Bioinformatics Laboratory <cb2labs@gmail.com>
2016-05-10 18:09:59 +0530

    Initial commit
    Segment®: A little JavaScript class (without dependencies) to draw and animate SVG path strokes.
    Help: https://github.com/lmgonzalves/segment

---
+++ source/core/script/plugin/segment.js
@@ -0,0 +1,9 @@
+/*!
+ * ~~~~~~~~
+ * Segment®   : A little JavaScript class to draw and animate SVG path strokes.
+ * __________________________________________________________________________________
+ * @copyright : 2015, lmgonzalves, All Rights Reserved.
+ * @help      : https://github.com/lmgonzalves/segment
+ * @license   : MIT
+ * __________________________________________________________________________________
+ */
