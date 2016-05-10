49b3736928e61c8ad60ffc811fa6021c3c75a62d
Computational Biology & Bioinformatics Laboratory <cb2labs@gmail.com>
2016-05-10 18:01:00 +0530

    Initial commit
    classList®: Cross-browser full element.classList implementation.
    Help: https://github.com/eligrey/classList.js | http://eligrey.com/.

---
+++ source/core/script/plugin/classList.js
@@ -0,0 +1,14 @@
+/*!
+ * ~~~~~~~~~~
+ * classList® : Cross-browser full "element.classList" implementation.
+ * __________________________________________________________________________________
+ * @copyright : 2014 - 2016, Eli Grey, All Rights Reserved.
+ * @help      : https://github.com/eligrey/classList.js | http://eligrey.com/
+ * @license   : http://unlicense.org/
+ * __________________________________________________________________________________
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/]
+ * @copyright : Sequømics Corporation [http://sequomics.com/]
+ * __________________________________________________________________________________
+ */
