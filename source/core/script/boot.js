273105b830d2355646a037c29dcde4b8d761f10c
Computational Biology & Bioinformatics Laboratory <cb2labs@gmail.com>
2016-05-10 16:18:03 +0530

    Initial commit
    boot.js, The Non-AMD JavaScript - for defining "CBBL®" configurations.

---
+++ source/core/script/boot.js
@@ -0,0 +1,21 @@
+/*!
+ * CBBL®: Computational Biology and Bioinformatics Laboratory.
+ * _______________________________________________________________
+ * CBBL®, https://github.com/CB2L
+ * The Non-AMD JavaScript - for defining "CBBL®" configurations.
+ * _______________________________________________________________
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/]
+ * @copyright : Sequømics Corporation [http://sequomics.com/]
+ * _______________________________________________________________
+ * @date      : 10-May-2016
+ * @license   : Apache, version 2.0
+ * @require   : Node.js®
+ * @require   : NPM
+ * @require   : Grunt
+ * _______________________________________________________________
+ *
+ * --/The Heart of Non-AMD System/-- of "CBBL®".
+ * _______________________________________________________________
+ */
