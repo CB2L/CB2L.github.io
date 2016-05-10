6747cb3fbe1f3e28f544c9d8c629b8322cdeb15e
Computational Biology & Bioinformatics Laboratory <cb2labs@gmail.com>
2016-05-10 18:15:15 +0530

    Initial commit
    async®: An AMD plugin for asynchronously dependency loading.
    Help: https://github.com/millermedeiros/requirejs-plugins

---
+++ source/core/script/helpers/async.js
@@ -0,0 +1,15 @@
+/*!
+ * ~~~~~~
+ * async®     : An AMD plugin for asynchronously dependency loading.
+ * _________________________________________________________________
+ * @copyright : 2014, Miller Medeiros. All Rights Reserved.
+ * @help      : https://github.com/millermedeiros/requirejs-plugins
+ * @require   : requirejs
+ * @license   : MIT license.
+ * _________________________________________________________________
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/]
+ * @copyright : Sequømics Corporation [http://sequomics.com/]
+ * _________________________________________________________________
+ */
