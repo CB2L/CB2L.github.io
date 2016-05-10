84d05de17b6ed12c275b8f1436b1c814e6bc68de
Computational Biology & Bioinformatics Laboratory <cb2labs@gmail.com>
2016-05-10 18:04:02 +0530

    Initial commit
    d3-ease®: Easing functions for smooth animation.
    Help: https://github.com/d3/d3-ease

---
+++ source/core/script/plugin/d3.ease.js
@@ -0,0 +1,14 @@
+/*!
+ * ~~~~~~~~
+ * d3-ease®   : Easing functions for smooth animation.
+ * ___________________________________________________________________________
+ * @copyright : 2010 - 2016, Mike Bostock, All Rights Reserved.
+ * @help      : https://github.com/d3/d3-ease
+ * @license   : MIT
+ * ___________________________________________________________________________
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/]
+ * @copyright : Sequømics Corporation [http://sequomics.com/]
+ * ___________________________________________________________________________
+ */
