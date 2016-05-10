3680966987196b7402c87b3a305a146c6b7cad0d
Computational Biology & Bioinformatics Laboratory <cb2labs@gmail.com>
2016-05-10 21:18:02 +0530

    Initial commit
    Respond: A fast & lightweight polyfill for min/max-width CSS3 Media Queries (for IE 6-8, and more).
    Help: https://github.com/scottjehl/Respond

---
+++ source/core/script/polyfills/respond/1.4.2/respond.js
@@ -0,0 +1,8 @@
+/*!
+ * Respond.js
+ * A fast & lightweight polyfill for min/max-width CSS3 Media Queries (for IE 6-8, and more).
+ * @copyright : 2012, Scott Jehl, All Rights Reserved.
+ * @help      : https://github.com/scottjehl/Respond
+ * @license   : MIT
+ * @url       : scottjehl@gmail.com | http://scottjehl.com/
+ */
