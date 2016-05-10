b490d5067ad117b8318fdfbd0cb5804e32b85f36
Computational Biology & Bioinformatics Laboratory <cb2labs@gmail.com>
2016-05-10 17:56:57 +0530

    Initial commit
    util.js®: A JavaScript plugin for Keyboard Input Utility.

---
+++ source/core/script/module/util.js
@@ -0,0 +1,15 @@
+/*!
+ * ~~~~~~~~
+ * util.js®   : A JavaScript plugin for Keyboard Input Utility.
+ * ____________________________________________________________________
+ * @help      : https://github.com/iamprabhat/iamprabhat/kbdx
+ * @require   : requirejs
+ * @license   : MIT license.
+ * ____________________________________________________________________
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/].
+ * @copyright : Prabhat Kumar [http://prabhatkumar.org/].
+ * @date      : 10-04-2016
+ * ____________________________________________________________________
+ */
