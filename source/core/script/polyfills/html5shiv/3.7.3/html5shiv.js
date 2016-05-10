19b865f7f52c1f40fd45f808c47ee104b811f2b0
Computational Biology & Bioinformatics Laboratory <cb2labs@gmail.com>
2016-05-10 21:11:13 +0530

    Initial commit
    The HTML5 Shiv: The defacto way to enable use of HTML5 sectioning elements in legacy Internet Explorer.
    Help: https://github.com/aFarkas/html5shiv

---
+++ source/core/script/polyfills/html5shiv/3.7.3/html5shiv.js
@@ -0,0 +1,8 @@
+/*!
+ * The HTML5 Shiv
+ * The HTML5 Shiv enables use of HTML5 sectioning elements in legacy Internet Explorer and provides basic HTML5 styling for Internet Explorer 6-9, Safari 4.x (and iPhone 3.x), and Firefox 3.x.
+ * @copyright : 2015, Alexander Farkas, All Rights Reserved.
+ * @help      : https://github.com/aFarkas/html5shiv
+ * @license   : MIT
+ * @url       : http://paulirish.com/2011/the-history-of-the-html5-shiv/
+ */
