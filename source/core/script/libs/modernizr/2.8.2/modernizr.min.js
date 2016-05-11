aad17d74df9fc5f613415e9053dad936e402eae6
Computational Biology & Bioinformatics Laboratory <cb2labs@gmail.com>
2016-05-11 09:36:43 +0530

    Initial commit
    Modernizr is a JavaScript library that detects HTML5 and CSS3 features in the user’s browser.
    Help: https://github.com/modernizr/modernizr

---
+++ source/core/script/libs/modernizr/2.8.2/modernizr.min.js
@@ -0,0 +1,8 @@
+/*!
+ * Modernizr
+ * Modernizr is a JavaScript library that detects HTML5 and CSS3 features in the user’s browser.
+ * @copyright : 2009 - 2016, Faruk Ates, Paul Irish, and Alex Sexton, All Rights Reserved.
+ * @help      : https://github.com/modernizr/modernizr
+ * @license   : BSD and MIT
+ * @url       : https://modernizr.com/ | https://modernizr.com/license/
+ */
