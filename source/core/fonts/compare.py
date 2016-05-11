9813c11b1908fcdde8496b645f66b11f48c49469
Computational Biology & Bioinformatics Laboratory <cb2labs@gmail.com>
2016-05-11 10:24:50 +0530

    Initial commit
    compare.py, to compare fonts.

---
+++ source/core/fonts/compare.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__authors__       = [
+    '"Prabhat Kumar" <prabhat.genome@gmail.com>',
+    '"Sequømics Corporation" <admin@sequomics.com>'
+    ]
+__contributors__  = '"Emma Jörenberg" <https://github.com/jorenberg>'
+__license__       = 'Apache License'
